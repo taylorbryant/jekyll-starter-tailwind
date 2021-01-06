aa08a968d91cdc0f543e59debfbdc7857f60fb2f
sharathdt <sharath.dt@gmail.com>
2021-01-06 08:15:34 +0530

    remove custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
     extend: {},
-    fontFamily: {
-      'sans': ['IBM Plex Mono', 'monospace']
-    }
   },
   variants: {},
   plugins: [],
